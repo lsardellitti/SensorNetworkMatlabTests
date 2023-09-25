b6fe7b2d2525eee0d403540d9955e3cdffb4d193
Luca Sardellitti <16ls53@queensu.ca>
2023-09-25 16:27:01 -0400

    refactor using error matrix

--- ErrorTheta0HeatMap.m
+++ ErrorTheta0HeatMap.m
@@ -1,29 +1,18 @@
 theta = 0;
 numXVals = 1000;
-xSearchOffset = 5;
+xSearchOffset = 50;
 
 % Values to test over
-testPwVals = linspace(0.001,3,50);
-testPsVals = linspace(0.001,3,50);
+PwVals = linspace(0.001,3,50);
+PsVals = linspace(0.001,3,50);
 
-testLength = length(testPwVals)*length(testPsVals);
+errorVals = zeros(length(PsVals),length(PwVals));
 
-PwVals = ones(testLength,1);
-PsVals = ones(testLength,1);
-
-for i = 1:length(testPwVals)
-    for j = 1:length(testPsVals)
-        PwVals((i-1)*length(testPsVals) + j) = testPwVals(i);
-        PsVals((i-1)*length(testPsVals) + j) = testPsVals(j);
-    end
-end
-
-errorVals = zeros(testLength,1);
-
-for testIndex = 1:testLength
+for PwIndex = 1:length(PwVals)
+for PsIndex = 1:length(PsVals)
     % Comment out whichever parameter is being modified here in BaseSetup
-    Ps = PsVals(testIndex);
-    Pw = PwVals(testIndex);
+    Ps = PsVals(PsIndex);
+    Pw = PwVals(PwIndex);
 
     BaseSetup;
 
@@ -31,7 +20,8 @@ for testIndex = 1:testLength
     
     [errorVal, ~] = calculateErrorFromDR(x, points, P0, P1, pc0, pc1, noistdv);
     
-    errorVals(testIndex) = errorVal;
+    errorVals(PsIndex,PwIndex) = errorVal;
+end
 end
 
 % Error heat map P1 P2
@@ -41,9 +31,8 @@ levels = 1000;
 fontSize = 12;
 lineSize = 1;
 
-errorMatrix = reshape(errorVals, length(testPsVals), length(testPwVals));
 set(gca,'ColorScale','log')
-contourf(testPwVals, testPsVals, errorMatrix, levels, 'LineColor', 'none','HandleVisibility','off')
+contourf(PwVals, PsVals, errorVals, levels, 'LineColor', 'none','HandleVisibility','off')
 cBar = colorbar;
 cBar.TickLabelInterpreter = 'latex';
 cBar.Ticks = [0.02, 0.03, 0.05, 0.1, 0.2, 0.3];
@@ -51,11 +40,11 @@ cBar.TickLabels = {'$$2\times10^{-2}$$', '$$3\times10^{-2}$$', '$$5\times10^{-2}
 title(cBar, '$$P_e(P_1,P_2)$$', 'Interpreter', 'latex','FontSize',fontSize)
 
 if caseType == 3
-    PsTildeVals = (N0*P0*P1./(2*testPwVals))*log(((1-Ew-Es)^2-PRatio2*(1-Ew)*(1-Es)*Ew*Es)/((Es-Ew)^2-PRatio2*(1-Ew)*(1-Es)*Ew*Es));
-    plot(testPwVals, PsTildeVals, 'r', 'LineWidth', lineSize)
+    PsTildeVals = (N0*P0*P1./(2*PwVals))*log(((1-Ew-Es)^2-PRatio2*(1-Ew)*(1-Es)*Ew*Es)/((Es-Ew)^2-PRatio2*(1-Ew)*(1-Es)*Ew*Es));
+    plot(PwVals, PsTildeVals, 'r', 'LineWidth', lineSize)
     legend('$$\tilde{P}_2(P_1)$$', 'Interpreter', 'latex','FontSize',fontSize)
-    xlim([0 max(testPwVals)])
-    ylim([0 max(testPsVals)])
+    xlim([0 max(PwVals)])
+    ylim([0 max(PsVals)])
 end
 
 xlabel('$$P_1$$', 'Interpreter', 'latex','FontSize',fontSize) 
