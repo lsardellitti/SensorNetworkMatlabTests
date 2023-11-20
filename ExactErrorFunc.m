7d0fd59522fcb4b8cc52765e6c3ffb6ae0c5287c
Luca Sardellitti <16ls53@queensu.ca>
2023-11-20 16:09:24 -0500

    refactor for consistency

--- ExactErrorFunc.m
+++ ExactErrorFunc.m
@@ -1,10 +1,9 @@
-thetaVals = linspace(0,pi/2,50);
-errorVals = zeros(1, length(thetaVals));
-thetaDR = 0.5;
+testVals = linspace(-5,10,100);
+errorVals = zeros(1, length(testVals));
+% thetaDR = 0.5;
 
 % calculate exact error funtion for p=0.5, A=1
-for thetaIndex = 1:length(thetaVals)
-    theta = thetaVals(thetaIndex);
+for testIndex = 1:length(testVals)
     BaseSetup;
         
     a00pdf = @(a,b) mvnpdf([a.',b.'], a00, eye(2)*(N0/2)).';
@@ -13,8 +12,8 @@ for thetaIndex = 1:length(thetaVals)
     a11pdf = @(a,b) mvnpdf([a.',b.'], a11, eye(2)*(N0/2)).';
     
     % testing constant decision regions
-    theta =  thetaDR;
-    BaseSetup;
+%     theta =  thetaDR;
+%     BaseSetup;
     
     ybound = @(a) (N0/(2*Ps*sin(theta)))*(atanh(tanh(2*a*Pw/N0)*(K0+K1)/(K0-K1)) - 2*a*Ps*cos(theta)/N0);
     xBound = abs((N0/(2*Pw))*atanh((K0-K1)/(K0+K1)));
@@ -28,16 +27,16 @@ for thetaIndex = 1:length(thetaVals)
     end
     
     % testing constant decision regions, put back to normal
-    theta = thetaVals(thetaIndex);
-    BaseSetup;
+%     theta = testVals(thetaIndex);
+%     BaseSetup;
     
     e00 = integral2(a00pdf,-xBound,xBound,lowerYBound,upperYBound) + (1 - normcdf((a00(1) + xBound)/sqrt(N0/2)));
     e01 = integral2(a01pdf,-xBound,xBound,lowerYBound,upperYBound) + (1 - normcdf((a01(1) + xBound)/sqrt(N0/2)));
     e10 = integral2(a10pdf,-xBound,xBound,lowerYBound,upperYBound) + (1 - normcdf((a10(1) + xBound)/sqrt(N0/2)));
     e11 = integral2(a11pdf,-xBound,xBound,lowerYBound,upperYBound) + (1 - normcdf((a11(1) + xBound)/sqrt(N0/2)));
     
-    errorVals(thetaIndex) = p11g1*e11 + p10g1*e10 + p01g1*e01 + p00g1*e00;
+    errorVals(testIndex) = p11g1*e11 + p10g1*e10 + p01g1*e01 + p00g1*e00;
 end
 
 figure
-plot(thetaVals, errorVals);
+plot(testVals, errorVals);
