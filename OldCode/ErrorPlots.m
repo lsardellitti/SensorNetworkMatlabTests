dab4e1b12eeb2e953c6dcd1b6e0e9b69571a3d5d
lsardellitti <44039252+lsardellitti@users.noreply.github.com>
2024-08-14 15:00:27 -0400

    Reorganize Fodlers (#3)
    * remove old code
    
    * create two user sub folder

--- OldCode/ErrorPlots.m
+++
@@ -1,9 +0,0 @@
-dataDir = 'ErrorProbSimulatedData';
-theta = 0;
-BaseSetup;
-
-% data has thetaVals, errorCount, trialCount, errorProbs
-data = load(strcat(dataDir,sprintf('/N0%0.2fPw%0.2fPs%0.2fEw%0.2fEs%0.2f.mat',N0,Pw^2,Ps^2,Ew,Es)));
-
-figure
-plot(data.thetaVals, data.errorProbs);
